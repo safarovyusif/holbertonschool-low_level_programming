7c4a7ee8a9321735079ba51ce0b97faf8fbe6013
safarovyusif <safarov.yusif@yahoo.com>
2025-07-27 17:48:01 +0000

    task

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific message using puts function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
