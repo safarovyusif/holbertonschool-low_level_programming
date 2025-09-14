abc9f3e65fdaaca4b658e4175543892b4ccbc78c
safarovyusif <safarov.yusif@yahoo.com>
2025-09-14 16:52:45 +0000

    task

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,37 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
 #include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
 /**
- * create_file - function will create a file
- * @filename: name of file to be created
- * @text_content: text to be written in new file
- * Return: 1 on success, -1 on fail
+ * create_file - Creates a file and writes a string to it.
+ * @filename: The name of the file to create.
+ * @text_content: The string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int z, n_write;
+	int fd;
+	ssize_t bytes_written;
 
-    if (filename == NULL)
-        return (-1);
-    if (text_content == NULL)
-        text_content = "";
+	if (filename == NULL)
+		return (-1);
 
-    z = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-    if (z == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    n_write = write(z, text_content, strlen(text_content));
-    if (n_write == -1)
-    {
-        close(z);
-        return (-1);
-    }
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1 || (size_t)bytes_written != strlen(text_content))
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(z);
-    return (1);
+	close(fd);
+
+	return (1);
 }
-
