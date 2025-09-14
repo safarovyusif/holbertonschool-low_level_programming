fc41ec7a639d1429f030a2c0b59472a047e7f8f9
safarovyusif <safarov.yusif@yahoo.com>
2025-09-14 16:45:12 +0000

    task

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 #include "main.h"
+
 /**
  * create_file - function will create a file
- * @filename:  name of file to be created
+ * @filename: name of file to be created
  * @text_content: text to be written in new file
- * Return: function will return 1 on success, -1 on fail
+ * Return: 1 on success, -1 on fail
  */
 int create_file(const char *filename, char *text_content)
 {
-	int z, i = 0, n_write;
+    int z, n_write;
 
-	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		text_content = "";
-	z = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
-	if (z == -1)
-		return (-1);
-	while (text_content[i] != '\0')
-		i++;
-	n_write = write(z, text_content, i);
-	if (n_write == -1)
-		return (-1);
-	return (1);
+    if (filename == NULL)
+        return (-1);
+    if (text_content == NULL)
+        text_content = "";
+
+    z = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+    if (z == -1)
+        return (-1);
+
+    n_write = write(z, text_content, strlen(text_content));
+    if (n_write == -1)
+    {
+        close(z);
+        return (-1);
+    }
+
+    close(z);
+    return (1);
 }
+
